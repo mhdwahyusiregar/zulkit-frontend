767582cb9d4bef6334eb029a2a1d2c9cc17e3578
mhdwahyusiregar <siregarwahyu573@gmail.com>
2023-12-14 11:39:42 +0700

    feat: add pricing and succes page

--- src/components/Layouts/Navbar.vue
+++ src/components/Layouts/Navbar.vue
@@ -2,13 +2,19 @@
 import Logo from '@/components/Layouts/Logo.vue';
 import NavigationLinks from '@/components/Layouts/NavigationLinks.vue';
 import UserInfo from '@/components/Layouts/UserInfo.vue';
+import AuthButton from './AuthButton.vue';
 </script>
 
 <template>
-  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-    <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
+  <nav
+    class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800"
+  >
+    <div
+      class="container flex flex-wrap items-center justify-between mx-auto my-2"
+    >
       <Logo />
       <UserInfo />
+      <!-- <AuthButton /> -->
       <NavigationLinks />
     </div>
   </nav>
--- src/components/homepage/Categories.vue
+++ src/components/homepage/Categories.vue
@@ -7,16 +7,20 @@ const categories = ref([
   { id: 2, title: 'Fonts', count: 32, image: 'categories-2.jpg' },
   { id: 3, title: 'Icon set', count: 313, image: 'categories-3.jpg' },
   { id: 4, title: 'Website UI Kit', count: 75531, image: 'categories-4.jpg' },
-])
+]);
 </script>
 
 <template>
-  <div class="container px-4 mx-auto my-16 md:px-12">
+  <div class="container px-4 mx-auto my-16 md:px-12" id="categories">
     <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
-      <CategoryCard v-for="category in categories" :key="category.id" :title="category.title" :count="category.count"
-        :image="category.image" />
-
+      <CategoryCard
+        v-for="category in categories"
+        :key="category.id"
+        :title="category.title"
+        :count="category.count"
+        :image="category.image"
+      />
     </div>
   </div>
 </template>
